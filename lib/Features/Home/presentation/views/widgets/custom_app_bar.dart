b047a1453bcee677d55fb1bbf5c380aeacd594e9
Ahmed Adel <dola9240@gmail.com>
2024-08-16 04:24:14 +0300

    add custom list view item to home

--- lib/Features/Home/presentation/views/widgets/custom_app_bar.dart
+++ lib/Features/Home/presentation/views/widgets/custom_app_bar.dart
@@ -10,7 +10,7 @@ class CustomAppBar extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Padding(
-      padding: const EdgeInsets.symmetric(horizontal: 24, vertical: 50),
+      padding: const EdgeInsets.symmetric(horizontal: 24, vertical: 60),
       child: Row(
         children: [
           Image.asset(
---
+++ lib/Features/Home/presentation/views/widgets/custom_list_view_item.dart
@@ -0,0 +1,30 @@
+// ignore_for_file: prefer_const_constructors
+
+import 'package:ebook_app/core/utils/assets.dart';
+import 'package:flutter/material.dart';
+
+class CustomListViewItem extends StatelessWidget {
+  const CustomListViewItem({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: MediaQuery.of(context).size.height * .3,
+      child: AspectRatio(
+        aspectRatio: 2.7 / 4, // width / height.
+        child: Container(
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(16),
+            color: Colors.teal,
+            image: DecorationImage(
+              fit: BoxFit.fill,
+              image: AssetImage(
+                AssetsData.testImage,
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
