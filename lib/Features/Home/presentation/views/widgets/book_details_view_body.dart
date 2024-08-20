66babfcbb8e8f5d8550f36184258b10d5775676e
Ahmed Adel <dola9240@gmail.com>
2024-08-21 02:53:04 +0300

    add title and subtitle to details

--- lib/Features/Home/presentation/views/widgets/book_details_view_body.dart
+++ lib/Features/Home/presentation/views/widgets/book_details_view_body.dart
@@ -2,6 +2,7 @@
 
 import 'package:ebook_app/Features/Home/presentation/views/widgets/custom_book_details_app_bar.dart';
 import 'package:ebook_app/Features/Home/presentation/views/widgets/custom_book_item.dart';
+import 'package:ebook_app/core/utils/styles.dart';
 import 'package:flutter/material.dart';
 
 class BookDetailsViewBody extends StatelessWidget {
@@ -16,9 +17,31 @@ class BookDetailsViewBody extends StatelessWidget {
         children: [
           CustomBookDetailsAppBar(),
           Padding(
-            padding: EdgeInsets.symmetric(horizontal: width * .17),
+            padding: EdgeInsets.symmetric(horizontal: width * .2),
             child: CuatomBookImage(),
           ),
+          SizedBox(
+            height: 43,
+          ),
+          Text(
+            'The Jungle Book',
+            style: Styles.textStyle30.copyWith(
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+          SizedBox(
+            height: 6,
+          ),
+          Opacity(
+            opacity: .7,
+            child: Text(
+              'Rudyard Kipling',
+              style: Styles.textStyle18.copyWith(
+                fontStyle: FontStyle.italic,
+                fontWeight: FontWeight.w500,
+              ),
+            ),
+          )
         ],
       ),
     );
