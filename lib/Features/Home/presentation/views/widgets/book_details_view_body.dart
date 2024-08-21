4ed56b74cb461e4f2b91f1871040389c3ef94de0
Ahmed Adel <dola9240@gmail.com>
2024-08-21 21:00:23 +0300

    create similar books list view

--- lib/Features/Home/presentation/views/widgets/book_details_view_body.dart
+++ lib/Features/Home/presentation/views/widgets/book_details_view_body.dart
@@ -4,6 +4,7 @@ import 'package:ebook_app/Features/Home/presentation/views/widgets/book_rating.d
 import 'package:ebook_app/Features/Home/presentation/views/widgets/books_action.dart';
 import 'package:ebook_app/Features/Home/presentation/views/widgets/custom_book_details_app_bar.dart';
 import 'package:ebook_app/Features/Home/presentation/views/widgets/custom_book_item.dart';
+import 'package:ebook_app/Features/Home/presentation/views/widgets/similar_books_list_view.dart';
 import 'package:ebook_app/core/utils/styles.dart';
 import 'package:flutter/material.dart';
 
@@ -54,6 +55,22 @@ class BookDetailsViewBody extends StatelessWidget {
             height: 37,
           ),
           BooksAction(),
+          SizedBox(
+            height: 50,
+          ),
+          Align(
+            alignment: Alignment.centerLeft,
+            child: Text(
+              'You can also like',
+              style: Styles.textStyle14.copyWith(
+                fontWeight: FontWeight.w600,
+              ),
+            ),
+          ),
+          SizedBox(
+            height: 16,
+          ),
+          SimilarBooksListView(),
         ],
       ),
     );
