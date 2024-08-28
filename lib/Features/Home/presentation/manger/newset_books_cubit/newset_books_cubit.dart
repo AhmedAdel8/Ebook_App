804607ec19ac8782d818c366adcf76b5d6a95e37
Ahmed Adel <dola9240@gmail.com>
2024-08-28 22:32:46 +0300

    newset  books cubit done

--- lib/Features/Home/presentation/manger/newset_books_cubit/newset_books_cubit.dart
+++ lib/Features/Home/presentation/manger/newset_books_cubit/newset_books_cubit.dart
@@ -1,9 +1,30 @@
 import 'package:bloc/bloc.dart';
 import 'package:ebook_app/Features/Home/data/models/book_model/book_model.dart';
+import 'package:ebook_app/Features/Home/data/repos/home_repo.dart';
 import 'package:equatable/equatable.dart';
 
 part 'newset_books_state.dart';
 
 class NewsetBooksCubit extends Cubit<NewsetBooksState> {
-  NewsetBooksCubit() : super(NewsetBooksInitial());
+  NewsetBooksCubit(this.homeRepo) : super(NewsetBooksInitial());
+
+  final HomeRepo homeRepo;
+
+  Future<void> fetchNewsetBooks() async {
+    emit(NewsetBooksLoading());
+    var result = await homeRepo.fetchNewsetBooks();
+
+    result.fold(
+      (failure) {
+        emit(
+          NewsetBooksFailure(failure.errMessage),
+        );
+      },
+      (books) {
+        emit(
+          NewsetBooksSuccess(books),
+        );
+      },
+    );
+  }
 }
