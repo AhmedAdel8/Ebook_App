fd8c949fcae0b3613b234895641c374270771afd
Ahmed Adel <dola9240@gmail.com>
2024-08-30 15:59:53 +0300

    cached network image

--- lib/Features/Home/presentation/views/widgets/custom_book_item.dart
+++ lib/Features/Home/presentation/views/widgets/custom_book_item.dart
@@ -1,6 +1,4 @@
-// ignore_for_file: prefer_const_constructors
-
-import 'package:ebook_app/core/utils/assets.dart';
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 
 class CuatomBookImage extends StatelessWidget {
@@ -8,17 +6,16 @@ class CuatomBookImage extends StatelessWidget {
   final String imageUrl;
   @override
   Widget build(BuildContext context) {
-    return AspectRatio(
-      aspectRatio: 2.6 / 4, // width / height.
-      child: Container(
-        decoration: BoxDecoration(
-          borderRadius: BorderRadius.circular(16),
-          image: DecorationImage(
-            fit: BoxFit.fill,
-            image: NetworkImage(
-              imageUrl,
+    return ClipRRect(
+      borderRadius: BorderRadius.circular(16),
+      child: AspectRatio(
+        aspectRatio: 2.6 / 4, // width / height.
+        child: CachedNetworkImage(
+          fit: BoxFit.fill,
+          imageUrl: imageUrl,
+          errorWidget: (context, url, error) => const Icon(
+            Icons.error,
             ),
-          ),
         ),
       ),
     );
